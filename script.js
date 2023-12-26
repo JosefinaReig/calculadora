d68144dace1d88ebf15054de0a801f0b5f878429
Josefina <reigjosefina95@gmail.com>
2023-12-26 18:43:38 -0300

    Calculadora con JS

---
+++ script.js
@@ -0,0 +1,14 @@
+function agregar(valor){
+    document.getElementById('pantalla').value += valor
+
+}
+
+function borrar(){
+document.getElementById('pantalla').value = ''
+}
+
+function calcular(){
+    const valorEnPantalla= document.getElementById('pantalla').value 
+    const resultado= eval(valorEnPantalla) 
+    document.getElementById('pantalla').value = resultado
+}
